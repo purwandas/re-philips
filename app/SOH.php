cc2b9dcab4b126181d7ee80a25c8a408a6d3478e
purwandas <purwandas@gmail.com>
2017-10-06 11:20:54 +0700

    soh model, keapus tadi

---
+++ app/SOH.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Filters\QueryFilters;
+
+class Soh extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+    	'user_id','store_id','date'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /* Metode tambahan untuk model. */
+
+    /**
+     * Relation Method(s).
+     *
+     */
+
+    public function sohDetails()
+    {
+        return $this->hasMany('App\SOHDetail', 'soh_id');
+    }
+
+    public function store()
+    {
+        return $this->belongsTo('App\Store', 'store_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    /**
+     * Filtering Berdasarakan Request User
+     * @param $query
+     * @param QueryFilters $filters
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeFilter($query, QueryFilters $filters)
+    {
+        return $filters->apply($query);
+    }
+
+}
