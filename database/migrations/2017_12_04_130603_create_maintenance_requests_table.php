8337a3511751606d0668f1515777ba59df278db2
purwandas <purwandas@gmail.com>
2017-12-05 16:59:13 +0700

    Maintenance Report 1. migration 2. model 3. api 4. report (otw filter)

---
+++ database/migrations/2017_12_04_130603_create_maintenance_requests_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaintenanceRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenance_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('region_id')->unsigned();
+            $table->foreign('region_id')->references('id')->on('regions');
+            $table->integer('area_id')->unsigned();
+            $table->foreign('area_id')->references('id')->on('areas');
+            $table->integer('store_id')->unsigned();
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->enum('category', array('KA', 'GC', 'PC', 'MCC'));
+            $table->enum('channel', array('TR', 'MR'));
+            $table->enum('type', array('RAK', 'POSM'));
+            $table->string('report');
+            $table->date('date');
+            $table->text('photo');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenance_requests');
+    }
+}
