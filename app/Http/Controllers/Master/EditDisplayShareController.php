5314ef725a54e2d64433534437b3f1808a78147e
purwandas <purwandas@gmail.com>
2017-12-29 18:02:12 +0700

    edit sales transaction 1. view blade + modal 2. controller 3. js 4. SalesTrait

---
+++ app/Http/Controllers/Master/EditDisplayShareController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Yajra\Datatables\Facades\Datatables;
+use App\Traits\StringTrait;
+use App\Traits\SalesTrait;
+use DB;
+use App\DisplayShare;
+use App\DisplayShareDetail;
+use App\Filters\DisplayShareFilters;
+
+class EditDisplayShareController extends Controller
+{
+    use StringTrait;
+    use SalesTrait;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('master.displayshare');
+    }
+
+    /**
+     * Data for DataTables
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function masterDataTable(){
+
+        $data = DisplayShare::
+                    where('display_shares.deleted_at', null)
+                    ->where('display_share_details.deleted_at', null)
+        			->join('display_share_details', 'display_shares.id', '=', 'display_share_details.display_share_id')
+                    ->join('stores', 'display_shares.store_id', '=', 'stores.id')
+                    ->join('users', 'display_shares.user_id', '=', 'users.id')
+                    ->join('categories', 'display_share_details.category_id', '=', 'categories.id')
+                    ->select('display_shares.week as week', 'users.name as user_name', 'users.nik as user_nik', 'stores.store_id as store_id', 'stores.store_name_1 as store_name_1', 'stores.store_name_2 as store_name_2', 'stores.dedicate as dedicate', 'display_share_details.id as id', 'display_share_details.philips as philips', 'display_share_details.all as all', 'categories.name as category')->get();
+
+        return $this->makeTable($data);
+    }
+
+    // Datatable template
+    public function makeTable($data){
+
+           return Datatables::of($data)
+           		->addColumn('action', function ($item) {
+
+                   return
+                    "<a href='#editdisplayshare' data-id='".$item->id."' data-toggle='modal' class='btn btn-sm btn-warning edit-displayshare'><i class='fa fa-pencil'></i></a>
+                    <button class='btn btn-danger btn-sm btn-delete deleteButton' data-toggle='confirmation' data-singleton='true' value='".$item->id."'><i class='fa fa-remove'></i></button>";
+
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = DisplayShareDetail::where('id', $id)->first();
+
+        return response()->json($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'philips' => 'required',
+            'all' => 'required',
+            ]);
+
+        $this->updateDisplayShare($id, $request['philips'], $request['all']);
+        return response()->json(
+            ['url' => url('/editdisplayshare'), 'method' => $request->_method]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+        $this->deleteDisplayShare($id);
+
+        return response()->json($id);
+    }
+}
--- app/Traits/SalesTrait.php
+++ app/Traits/SalesTrait.php
@@ -10,6 +10,10 @@ use App\SellOut;
 use App\SellOutDetail;
 use App\Reports\SummaryRetDistributor;
 use App\Reports\SummaryRetConsument;
+use App\Reports\SummaryFreeProduct;
+use App\Reports\SummaryTbat;
+use App\Reports\SummarySoh;
+use App\Reports\SummaryDisplayShare;
 use App\RetDistributor;
 use App\RetDistributorDetail;
 use App\RetConsument;
@@ -18,6 +22,12 @@ use App\FreeProduct;
 use App\FreeProductDetail;
 use App\Tbat;
 use App\TbatDetail;
+use App\Soh;
+use App\SohDetail;
+use App\DisplayShare;
+use App\DisplayShareDetail;
+use App\PosmActivity;
+use App\PosmActivityDetail;
 
 trait SalesTrait {
 
@@ -105,6 +115,225 @@ trait SalesTrait {
                         'quantity'=> $qty,
                         'value'=> $value,
                     ]);
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function deleteFreeProduct($id){
+
+        // Find Detail then delete
+        $detail = FreeProductDetail::where('id',$id)->first();
+        // return response()->json($detail);
+            $headerId = $detail->freeproduct_id;
+            
+        $detail->delete();
+        $summary = SummaryFreeProduct::where('freeproduct_detail_id',$id)->delete();
+
+            // Check if no detail exist delete header
+            $header = FreeProduct::where('id',$headerId)->first();
+            $details = FreeProductDetail::where('freeproduct_id',$header->id)->get();
+
+                if($details->count() == 0){
+                    $header->delete();
+                }
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+    public function updateFreeProduct($id, $qty){
+
+        $detail = FreeProductDetail::where('id',$id)->update(['quantity'=> $qty]);
+
+        $summary = SummaryFreeProduct::where('freeproduct_detail_id',$id)
+            ->first();
+            $value = $summary->unit_price * $qty;
+
+            $summary->update([
+                        'quantity'=> $qty,
+                        'value'=> $value,
+                    ]);
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function deleteTbat($id){
+
+        // Find Detail then delete
+        $detail = TbatDetail::where('id',$id)->first();
+        // return response()->json($detail);
+            $headerId = $detail->tbat_id;
+            
+        $detail->delete();
+        $summary = SummaryTbat::where('tbat_detail_id',$id)->delete();
+
+            // Check if no detail exist delete header
+            $header = Tbat::where('id',$headerId)->first();
+            $details = TbatDetail::where('tbat_id',$header->id)->get();
+
+                if($details->count() == 0){
+                    $header->delete();
+                }
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+    public function updateTbat($id, $qty){
+
+        $detail = TbatDetail::where('id',$id)->update(['quantity'=> $qty]);
+
+        $summary = SummaryTbat::where('tbat_detail_id',$id)
+            ->first();
+            $value = $summary->unit_price * $qty;
+
+            $summary->update([
+                        'quantity'=> $qty,
+                        'value'=> $value,
+                    ]);
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function deleteSoh($id){
+
+        // Find Detail then delete
+        $detail = SohDetail::where('id',$id)->first();
+        // return response()->json($detail);
+            $headerId = $detail->soh_id;
+            
+        $detail->delete();
+        $summary = SummarySoh::where('soh_detail_id',$id)->delete();
+
+            // Check if no detail exist delete header
+            $header = Soh::where('id',$headerId)->first();
+            $details = SohDetail::where('soh_id',$header->id)->get();
+
+                if($details->count() == 0){
+                    $header->delete();
+                }
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+    public function updateSoh($id, $qty){
+
+        $detail = SohDetail::where('id',$id)->update(['quantity'=> $qty]);
+
+        $summary = SummarySoh::where('soh_detail_id',$id)
+            ->first();
+            $value = $summary->unit_price * $qty;
+
+            $summary->update([
+                        'quantity'=> $qty,
+                        'value'=> $value,
+                    ]);
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function deleteDisplayShare($id){
+
+        // Find Detail then delete
+        $detail = DisplayShareDetail::where('id',$id)->first();
+        // return response()->json($detail);
+            $headerId = $detail->display_share_id;
+            
+        $detail->delete();
+        $summary = SummaryDisplayShare::where('displayshare_detail_id',$id)->delete();
+
+            // Check if no detail exist delete header
+            $header = DisplayShare::where('id',$headerId)->first();
+            $details = DisplayShareDetail::where('display_share_id',$header->id)->get();
+
+                if($details->count() == 0){
+                    $header->delete();
+                }
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+    public function updateDisplayShare($id, $philips, $all){
+
+        $detail = DisplayShareDetail::where('id',$id)->update(['philips'=> $philips, 'all'=> $all]);
+
+        $summary = SummaryDisplayShare::where('displayshare_detail_id',$id)
+            ->first();
+
+            $summary->update([
+                        'philips'=> $philips,
+                        'all'=> $all,
+                    ]);
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function deletePosmActivity($id){
+
+        // Find Detail then delete
+        $detail = PosmActivityDetail::where('id',$id)->first();
+        // return response()->json($detail);
+            $headerId = $detail->posmactivity_id;
+            
+        $detail->delete();
+
+            // Check if no detail exist delete header
+            $header = PosmActivity::where('id',$headerId)->first();
+            $details = PosmActivityDetail::where('posmactivity_id',$header->id)->get();
+
+                if($details->count() == 0){
+                    $header->delete();
+                }
+
+        if ($detail) {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+    public function updatePosmActivity($id, $quantity){
+
+        $detail = PosmActivityDetail::where('id',$id)->update(['quantity'=> $quantity]);
 
         if ($detail) {
             return true;
