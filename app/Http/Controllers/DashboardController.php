7e52991c1fa1ea6ac2dcc6101d7f9444dca3d589
darilshield <dirgantaradwiyoga@gmail.com>
2018-01-09 10:14:10 +0700

    Dashboard Module

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\AchievementTrait;
 use Illuminate\Http\Request;
 use Auth;
 
 class DashboardController extends Controller
 {
+    use AchievementTrait;
+
     /**
      * Display a listing of the resource.
      *
@@ -22,4 +25,19 @@ class DashboardController extends Controller
         return redirect('/logout');
     }
 
+    public function getDataNational(){
+        return $this->dataNational();
+    }
+
+    public function getDataRegion(){
+        return $this->dataRegion();
+    }
+
+    public function getDataArea(){
+        return $this->dataArea();
+    }
+
+    public function getDataSupervisor(){
+        return $this->dataSupervisor();
+    }
 }
