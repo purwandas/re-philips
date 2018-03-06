0ea99bc32b936f8f0836e5f52501690b6f09ad13
sadatech <purwandas@gmail.com>
2018-03-06 17:15:14 +0700

    sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -153,6 +153,7 @@
             <li class="nav-item 
                 {{ Request::is('target') ? 'active open' : '' }}
                 {{ Request::is('productfocus') ? 'active open' : '' }}
+                {{ Request::is('productpromo') ? 'active open' : '' }}
                 {{ Request::is('targetsalesman') ? 'active open' : '' }}
                 {{ Request::is('productfocussalesman') ? 'active open' : '' }}
             ">
